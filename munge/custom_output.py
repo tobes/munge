c79fbdea82196260874870293a0d09a434a02687
Toby Dacre <toby.junk@gmail.com>
2015-12-27 19:17:28 +0000

    add custom_output.py

---
+++ munge/custom_output.py
@@ -0,0 +1,13 @@
+from csv_util import make_csv
+
+
+# make_csv(
+#     'c_la_sub.csv',
+#     ''' SELECT
+#         "LAU215CD" AS code,
+#         "LAU215NM" AS desc
+#         FROM
+#         test_import;
+#     ''',
+#     table='test_import'
+# )
