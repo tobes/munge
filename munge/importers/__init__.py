ee08a7a44a81168f0af3728429c395d96c31781d
Toby Dacre <toby.junk@gmail.com>
2016-01-06 16:23:08 +0000

    __all__ added to importers.__init__

--- munge/importers/__init__.py
+++ munge/importers/__init__.py
@@ -1,9 +1,14 @@
 import os
 
+__all__ = []
+
 for module in os.listdir(os.path.dirname(__file__)):
     if module == '__init__.py' or module[-3:] != '.py':
         continue
-    __import__(module[:-3], locals(), globals())
+    name = module[:-3]
+    __import__(name, locals(), globals())
+    __all__.append(name)
 
 del module
+del name
 del os
