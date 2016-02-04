3c4c8b3136547df45a6b4dcd3820984edf763171
Toby Dacre <toby.junk@gmail.com>
2016-02-04 12:22:02 +0000

    update importer codes

--- munge/importers/codes.py
+++ munge/importers/codes.py
@@ -2,10 +2,11 @@ from munge.csv_util import import_all, csv_table_list
 
 
 DIRECTORY = 'codes'
+IMPORTER = 'codes'
 
 
 def importer(verbose=0):
-    import_all(DIRECTORY, verbose=verbose, keep_table=True)
+    import_all(DIRECTORY, verbose=verbose, keep_table=True, importer=IMPORTER)
 
 
 def tables():
