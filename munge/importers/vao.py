af4e64ab07f3edd2e2ae830df572879c19efd413
Toby Dacre <toby.junk@gmail.com>
2016-09-05 00:56:32 +0100

    vao map view updates 2

--- munge/importers/vao.py
+++ munge/importers/vao.py
@@ -1360,7 +1360,7 @@ AND a.la_code = la.la_code
     {
         'name': 'v_map_premises_names_sg',
         'sql': '''
-            SELECT v.uarn, scat_group_code
+            SELECT v.uarn, v.scat_group_code
             FROM
             (SELECT count(uarn) count,
             lat, long, scat_group_code
@@ -1376,7 +1376,7 @@ AND a.la_code = la.la_code
     {
         'name': 'v_map_premises_names_sc',
         'sql': '''
-            SELECT v.uarn, scat_code
+            SELECT v.uarn, v.scat_code
             FROM
             (SELECT count(uarn) count,
             lat, long, scat_code
